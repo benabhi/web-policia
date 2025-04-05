56d744583c345ffcfc1108d49feafe673ac9132d
Hernan Jalabert <benabhi@gmail.com>
2025-04-05 20:36:05 +0000

    guardado

--- lib/policia_web/components/custom_components.ex
+++ lib/policia_web/components/custom_components.ex
@@ -1441,25 +1441,138 @@ defmodule PoliciaWeb.CustomComponents do
 
   # Componente para tarjetas de la barra lateral
   attr :title, :string, default: "Enlaces de interés", doc: "Título de la barra lateral"
+
+  attr :icon, :string,
+    default: nil,
+    doc: "Ícono opcional para el encabezado (admin, users, shield, etc.)"
+
+  attr :highlight, :boolean, default: false, doc: "Si se debe destacar visualmente (estilo admin)"
+
+  attr :highlight_color, :string,
+    default: nil,
+    doc: "Color para destacar (si es diferente al tema del sitio)"
+
+  attr :class, :string, default: "", doc: "Clases CSS adicionales"
+
+  attr :role_name, :string,
+    default: nil,
+    doc: "Nombre del rol para mostrar (cuando highlight=true)"
+
   slot :inner_block, required: true, doc: "Contenido de la barra lateral"
 
   def sidebar_box(assigns) do
     assigns =
       assigns
-      |> assign(
-        :color_theme,
-        Config.webpage_theme()
-      )
+      |> assign(:color_theme, Config.webpage_theme())
+      |> assign_new(:highlight_color, fn -> assigns.color_theme end)
 
     ~H"""
-    <div class={"bg-white rounded-lg shadow-md border border-#{@color_theme}-100 overflow-hidden"}>
-      <div class={"bg-#{@color_theme}-900 text-white p-4"}>
-        <h2 class="font-semibold text-lg">{@title}</h2>
+    <%= if @highlight do %>
+      <!-- Versión destacada del box (estilo admin) -->
+      <div class={"bg-white rounded-lg shadow-md overflow-hidden border-2 border-#{@highlight_color}-400 #{@class}"}>
+        <div class={"bg-gradient-to-r from-#{@highlight_color}-600 to-#{@highlight_color}-700 text-white p-4 flex items-center"}>
+          <%= if @icon do %>
+            <%= case @icon do %>
+              <% "users" -> %>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                </svg>
+              <% "shield" -> %>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              <% "key" -> %>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 8a6 6 0 01-7.743 5.743L10 14l-1 1-1 1H6v2H2v-4l4.257-4.257A6 6 0 1118 8zm-6-4a1 1 0 100 2 2 2 0 012 2 1 1 0 102 0 4 4 0 00-4-4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              <% "lock" -> %>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              <% "admin" -> %>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              <% _ -> %>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+            <% end %>
+          <% end %>
+          <div class="flex-1">
+            <h2 class="font-semibold text-lg">{@title}</h2>
+            <%= if @role_name do %>
+              <span class={"text-xs font-medium text-#{@highlight_color}-100"}>
+                Rol: {@role_name}
+              </span>
+            <% end %>
+          </div>
+        </div>
+        <div class={"p-4 border-t-2 border-#{@highlight_color}-100"}>
+          {render_slot(@inner_block)}
+        </div>
       </div>
-      <div class="p-4">
-        {render_slot(@inner_block)}
+    <% else %>
+      <!-- Versión estándar original del box -->
+      <div class={"bg-white rounded-lg shadow-md border border-#{@color_theme}-100 overflow-hidden #{@class}"}>
+        <div class={"bg-#{@color_theme}-900 text-white p-4"}>
+          <h2 class="font-semibold text-lg">{@title}</h2>
+        </div>
+        <div class="p-4">
+          {render_slot(@inner_block)}
+        </div>
       </div>
-    </div>
+    <% end %>
     """
   end
 
